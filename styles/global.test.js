75922a34e045b08cb084e9de0a89df26a49711fc
Carson Bruce <carson@getproperly.com>
2019-01-31 20:40:43 +1300

    intial unit test framework

--- styles/global.test.js
+++ styles/global.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { render } from "enzyme";
+
 import GlobalCSS from "./global";
 
 it("should render the global styles and check it against a snapshot", () => {
-  const tree = renderer.create(<GlobalCSS />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = render(<GlobalCSS />);
+  expect(component).toMatchSnapshot();
 });
